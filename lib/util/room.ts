88f1217cfdb216f9a46d47f711bb3196486b76cd
Varun Rajkumar <pi.fermet@gmail.com>
2021-01-28 17:54:24 -0800

    commit

--- lib/util/room.ts
+++ lib/util/room.ts
@@ -225,7 +225,6 @@ export class Room extends EventEmitter {
                   } else {
                     this.startPacketGroupBroadcastToAll();
                     connection.player.setName(pd[0].PlayerName);
-                    console.log(pd[0].PlayerName); 
                     connection.player.setColor(pd[0].Color);
                     connection.player.setHat(pd[0].HatID);
                     connection.player.setPet(pd[0].PetID);
